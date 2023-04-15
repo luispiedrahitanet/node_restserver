05b5a2d2d8977dee16c9cfa573c559f34125f2d3
Luis Piedrahita <luispiedrahita.net@gmail.com>
2023-04-15 13:07:17 -0500

    Cambio de version a node 16

--- database/config.js
+++ database/config.js
@@ -6,7 +6,7 @@ const dbConnection = async () => {
         
         await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
             // useCreateIndex: true,
             // useFindAndModify: false
         });
